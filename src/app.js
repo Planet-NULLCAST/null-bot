ea422d74497f84ed5b7afc38ee3b915a75fa5af4
mohammed fayez <fayezmohammed23@gmail.com>
2021-12-25 04:42:36 +0000

    chore: refactor the folder structure

--- src/app.js
+++ src/app.js
@@ -1,46 +1,4 @@
-import client from './config/discord-config.js'
+import discordBot from '../bot/app.js';
 
-import welcomeMsg from './controllers/welcomeMsg.controller.js';
-import regulationsDm from './controllers/regulationsDm.controller.js';
-import envValidity from '../envValidate.js'
-import errorHandlerEmail from './services/errorHandlerEmail.js';
-
-// * to validate the minium requiment to run the applications.
-envValidity();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-
-client.on("guildMemberAdd" ,(message, member) => {
-  welcomeMsg({message: message});
-  regulationsDm({message: message});
-});
-
-
-// ! error handler for our bot.
-
-client.on('error', (error) => {
-  console.log('error', error);
-
-  errorHandlerEmail({error: error, handlerName: 'discord error'});
-})
-
-client.on('shardError', error => {
-	console.error('A websocket connection encountered an error:', error);
-
-  errorHandlerEmail({error: error, handlerName: 'websocket error'});
-});
-
-
-process.on('unhandledRejection', error => {
-	console.error('Unhandled promise rejection:', error);
-
-  errorHandlerEmail({error: error, handlerName: 'node unhandle error'});
-});
-
-process.on('uncaughtException', error => {
-	console.error('Unhandled promise rejection:', error);
-
-  errorHandlerEmail({error: error, handlerName: 'node uncaughtException error'});
-});
+// initialze the discord bot
+discordBot();
